dc1d3f6997138305c926257006d317974b02750a
Vitali Sasnouski <sgvitaly@gmail.com>
2018-07-29 11:07:54 +0300

    Sample output and update packages
    + add sample code to test the output;
    + update angularfire package due to build errors.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+
+export interface Item {
+  title: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'read-later';
+  private itemCollection: AngularFirestoreCollection<Item>;
+
+  items: Observable<Item[]>;
+  newItem: Item = { title: '', url: '' };
+
+  constructor(private readonly afs: AngularFirestore) {
+    this.itemCollection = afs.collection<Item>('articles');
+    this.items = this.itemCollection.valueChanges();
+  }
+
+  addItem(item: Item) {
+    this.itemCollection.add(item);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
